f5aba80ecddc9338d9612e0f4681783663f62365
Bhupendra Singh <bs4004908@gmail.com>
2026-06-27 10:15:00 +0530

    utils

--- eCartReact/src/component/Card/Card.jsx
+++ eCartReact/src/component/Card/Card.jsx
@@ -1,13 +1,26 @@
 import React from 'react'
 import styles from './Card.module.css'
 
-const Card = ({theme}) => {
+const Card = ({ theme, item }) => {
 
     return (
-    <div className={styles.card}>
-    Card
-    </div>
-)
+        <div key={item.id} className={[styles.card, theme === "light" ? styles.cardLight : styles.cardDark].join(' ')}>
+            <div className={styles.imageBlock} >
+                <img className={styles.Image} src={item.imageLink} />
+            </div>
+
+            <div className={theme === "light" ? styles.DescBlockLight : styles.DescBlockDark}>
+             <h3>{item.name}</h3>
+              <p className={styles.itemDesc}>{item.shortDesc}</p>
+                
+                <div className={styles.detail}>
+                    <div className={styles.price}>$ {item.price}</div>
+                    <div className={theme === "light" ? styles.btnLight : styles.btnDark} >Add To Cart</div>
+                </div>
+
+            </div>
+        </div>
+    )
 }
 
 export default Card
--- eCartReact/src/component/Home/Home.jsx
+++ eCartReact/src/component/Home/Home.jsx
@@ -1,8 +1,10 @@
-import React, { useContext } from 'react'
+import  { useContext } from 'react'
 import styles from './Home.module.css'
 import { ThemeContext } from '../../ThemeContext'
 import Card from '../Card/Card';
-
+import product from '../../utils/product.json'
+import RecCard from '../RecommendedCard/RecCard'
+import recommendedItem from '../../utils/recommendedItem.json';
 
 
 const Home = () => {
@@ -19,13 +21,48 @@ const Home = () => {
                     <h2>Trending Items</h2>
                     <div className={styles.productHorizontalList}>
 
-                        <Card  theme={theme} />
+                        {
+                            product.map((item, id) => {
+                                return (
+                                    <Card theme={theme} item={item} />
+                                );
+                            })
+                        }
 
                     </div>
                 </div>
+
+             <div>
+                <h2>Recommended Items</h2>
+                <div className={styles.recommendedBlock}>
+                    {
+                        recommendedItem.map((item)=>{
+                            return(
+                                <RecCard theme={theme} item={item} />
+                            );
+                        })
+                    }
+                    
+                </div>
             </div>
 
+            <div>
+                <h2>Trending Items</h2>
+                <div className={styles.productHorizontalList}>
 
+                    {
+                        product.reverse().map((item,id)=>{
+                            return(
+                                <Card theme={theme} item={item} />
+                            );
+                        })
+                    }
+
+
+                </div>
+            </div>
+
+            </div>
         </div>
     )
 }
